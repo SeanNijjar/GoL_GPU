ed1217cba57b4504614a25087e359fb3686f0c69
Sean <sean.nijjar@gmail.com>
2014-12-10 00:52:37 -0500

    pushed the lookup table into constant memory

--- boardGen.py
+++ boardGen.py
@@ -3,12 +3,14 @@ import sys
 
 f = open('input', 'w')
 a = []
-b = int(sys.argv[1]) * int(sys.argv[1])
+b = long(sys.argv[1]) * int(sys.argv[1])
+c = long(sys.argv[1])
 
-for i in range(0, b):
-    if(random.random() < 0.3):
-        f.write('1')
-    else:
-        f.write('0')
-
+for j in range(0, c):
+	for i in range(0, c):
+	    if(random.random() < 0.3):
+	        f.write('1')
+	    else:
+	        f.write('0')
+	f.write('\n')
 f.close()
